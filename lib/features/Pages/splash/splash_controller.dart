fd6c0a6e37befed31954ec72149550580d82a4b2
m.javad javadpoor <m.j.j.p1364@gmail.com>
2025-06-10 12:18:02 +0330

    add check connectivity

--- lib/features/Pages/splash/splash_controller.dart
+++ lib/features/Pages/splash/splash_controller.dart
@@ -3,57 +3,85 @@ import 'package:ecommerce_besinior/commons/utils/customSnackBar.dart';
 import 'package:ecommerce_besinior/features/Pages/HomePage/home_view.dart';
 import 'package:ecommerce_besinior/core/dependency/dependences.dart';
 import 'package:get/get.dart';
-// import 'package:connectivity_plus/connectivity_plus.dart'; // Uncomment if you want to use connectivity package
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class SplashController extends GetxController {
+  RxBool connectivity = false.obs;
+
   @override
   onInit() {
     super.onInit();
-    checkInternetConnection();
+    checkConnectivity1();
+
+  }
+
+  Future<void> checkConnectivity1() async {
+
+
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult.contains(ConnectivityResult.mobile)) {
+      // Mobile network is available
+      connectivity.value = true;
+      checkInternetConnection();
+    } else if (connectivityResult.contains(ConnectivityResult.wifi)) {
+      // Wi-Fi is available
+      connectivity.value = true;
+      checkInternetConnection();
+    } else {
+      // No available network types
+      connectivity.value = false;
+
+      CustomSnackBar.show(
+        title: 'خطا',
+        message: ' لطفا یکی از شبکه های ارتباطی را فعال کنید',
+      );
+    }
   }
 
   Future<void> checkInternetConnection() async {
     // Simulate a network check
     await Future.delayed(const Duration(seconds: 3));
     try {
-      final result = await InternetAddress.lookup('example.com');
-      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-        //Internet connection is available
-        Get.offAll(
-          () => HomeScreen(),
-          transition: Transition.fadeIn,
-          duration: const Duration(milliseconds: 500),
-          binding: AllBindings(),
-        );
-
-        /// Uncomment the following lines if you want to check specific connectivity types
-
-        // final List<ConnectivityResult> connectivityResult =
-        //     await (Connectivity().checkConnectivity());
-
-        // // This condition is for demo purposes only to explain every connection type.
-        // // Use conditions which work for your requirements.
-        // if (connectivityResult.contains(ConnectivityResult.mobile)) {
-        //   // Mobile network available.
-        // } else if (connectivityResult.contains(ConnectivityResult.wifi)) {
-        //   // Wi-fi is available.
-        //   // Note for Android:
-        //   // When both mobile and Wi-Fi are turned on system will return Wi-Fi only as active network type
-        // } else if (connectivityResult.contains(ConnectivityResult.ethernet)) {
-        //   // Ethernet connection available.
-        // } else if (connectivityResult.contains(ConnectivityResult.vpn)) {
-        //   // Vpn connection active.
-        //   // Note for iOS and macOS:
-        //   // There is no separate network interface type for [vpn].
-        //   // It returns [other] on any device (also simulator)
-        // } else if (connectivityResult.contains(ConnectivityResult.bluetooth)) {
-        //   // Bluetooth connection available.
-        // } else if (connectivityResult.contains(ConnectivityResult.other)) {
-        //   // Connected to a network which is not in the above mentioned networks.
-        // } else if (connectivityResult.contains(ConnectivityResult.none)) {
-        //   // No available network types
-        // }
+      if (connectivity.value) {
+        final result = await InternetAddress.lookup('example.com');
+        if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+          //Internet connection is available
+          Get.offAll(
+            () => HomeScreen(),
+            transition: Transition.fadeIn,
+            duration: const Duration(milliseconds: 500),
+            binding: AllBindings(),
+          );
+        }
       }
+
+      /// Uncomment the following lines if you want to check specific connectivity types
+
+      // final List<ConnectivityResult> connectivityResult =
+      //     await (Connectivity().checkConnectivity());
+
+      // This condition is for demo purposes only to explain every connection type.
+      // Use conditions which work for your requirements.
+      // if (connectivityResult.contains(ConnectivityResult.mobile)) {
+      // Mobile network available.
+      // } else if (connectivityResult.contains(ConnectivityResult.wifi)) {
+      // Wi-fi is available.
+      // Note for Android:
+      // When both mobile and Wi-Fi are turned on system will return Wi-Fi only as active network type
+      // } else if (connectivityResult.contains(ConnectivityResult.ethernet)) {
+      // Ethernet connection available.
+      // } else if (connectivityResult.contains(ConnectivityResult.vpn)) {
+      // Vpn connection active.
+      // Note for iOS and macOS:
+      // There is no separate network interface type for [vpn].
+      // It returns [other] on any device (also simulator)
+      // } else if (connectivityResult.contains(ConnectivityResult.bluetooth)) {
+      // Bluetooth connection available.
+      // } else if (connectivityResult.contains(ConnectivityResult.other)) {
+      // Connected to a network which is not in the above mentioned networks.
+      // } else if (connectivityResult.contains(ConnectivityResult.none)) {
+      // No available network types
+      // }
     } catch (e) {
       // No internet connection
       CustomSnackBar.show(title: 'خطا', message: 'به اینترنت متصل نیستید');
