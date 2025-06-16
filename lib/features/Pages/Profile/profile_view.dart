fd2ff0cd0a14cff30beaca6d2f28df5133ba98c5
m.javad javadpoor <m.j.j.p1364@gmail.com>
2025-06-16 11:17:09 +0330

    some

--- lib/features/Pages/Profile/profile_view.dart
+++ lib/features/Pages/Profile/profile_view.dart
@@ -6,7 +6,7 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Profile')),
+      appBar: AppBar(title: const Text('Profile'), centerTitle: true),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
