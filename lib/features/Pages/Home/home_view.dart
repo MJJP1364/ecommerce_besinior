be53df5d855d6570a6ae03c42cbe3f9e3259d8b6
m.javad javadpoor <m.j.j.p1364@gmail.com>
2025-06-16 09:55:49 +0330

    some change

--- lib/features/Pages/Home/home_view.dart
+++ lib/features/Pages/Home/home_view.dart
@@ -5,12 +5,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-      ),
-    );
-  } 
+    return Scaffold();
+  }
 }
